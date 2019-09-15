b033c902deaa6bb15658cd3841f82bf30b1bb8e3
cristianarbe <c.ariza@pm.me>
2019-09-15 22:38:04 +0100

    Change autostart
    Former-commit-id: 99b4b912a0abfe75076f11bf8443340c91d69486

--- .config/ms-windows/autostart.bat
+++ .config/ms-windows/autostart.bat
@@ -5,43 +5,39 @@ SET /P AREYOUSURE=Do you want to run startup programs?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 if exist "C:\Program Files (x86)\AutoProxySwitcher\AutoProxySwitcher.exe" (
-	start "" "C:\Program Files (x86)\AutoProxySwitcher\AutoProxySwitcher.exe"
+	start /min "" "C:\Program Files (x86)\AutoProxySwitcher\AutoProxySwitcher.exe"
 )
 
 if exist "C:\Program Files (x86)\Trend Micro\OfficeScan Client\PccNTMon.exe" (
-	start "" "C:\Program Files (x86)\Trend Micro\OfficeScan Client\PccNTMon.exe"
+	start /min "" "C:\Program Files (x86)\Trend Micro\OfficeScan Client\PccNTMon.exe"
 )
 
 if exist "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe" (
-	start "" "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe"
+	start /min "" "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe"
 )
 
 if exist "C:\Program Files\Everything\Everything.exe" (
-	start "" "C:\Program Files\Everything\Everything.exe"
+	start "" "C:\Program Files\Everything\Everything.exe" -minimized
 )
 
 if exist "C:\Program Files\pia_manager\pia_manager.exe" (
-	start "" "C:\Program Files\pia_manager\pia_manager.exe"
+	start /min "" "C:\Program Files\pia_manager\pia_manager.exe"
 )
 
-if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Skype for Business 2016.lnk" (
-	start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Skype for Business 2016.lnk"
+if exist "C:\ProgramData\Microsoft\Windows\start /min Menu\Programs\Skype for Business 2016.lnk" (
+	start /min "" "C:\ProgramData\Microsoft\Windows\start /min Menu\Programs\Skype for Business 2016.lnk"
 )
 
 if exist "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk" (
-	start "" "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk"
+	start /min "" "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk"
 )
 
 if exist "C:\Program Files\Private Internet Access\pia-client.exe" (
-	start "" "C:\Program Files\Private Internet Access\pia-client.exe"
+	start /min "" "C:\Program Files\Private Internet Access\pia-client.exe"
 )
 
 if exist "C:\Program Files\LGHUB\lghub.exe" (
-	start "" "C:\Program Files\LGHUB\lghub.exe"
-)
-
-if exist "C:\Program Files\Classic Shell\ClassicStartMenu.exe" (
-	start "" "C:\Program Files\Classic Shell\ClassicStartMenu.exe"
+	start /min "" "C:\Program Files\LGHUB\lghub.exe"
 )
 
 timeout 5
@@ -50,6 +46,9 @@ if exist "C:\Program Files\VeraCrypt\VeraCrypt.exe" (
 	start "" "C:\Program Files\VeraCrypt\VeraCrypt.exe"
 )
 
+if exist "C:\Users\%USERNAME%\.config\ms-windows\autostart-assistant.ahk" (
+	Start "" AutoHotkey.exe "C:\Users\%USERNAME%\.config\ms-windows\autostart-assistant.ahk"
+)
 
 :END
 endlocal
