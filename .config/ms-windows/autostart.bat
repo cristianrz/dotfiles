5fd39c77bb0f5ae55371ef94e525dfabb32adbb3
cristianarbe <c.ariza@posteo.net>
2019-12-02 14:43:33 +0000

    Mon, Dec  2, 2019  2:43:33 PM
    Former-commit-id: 2e1e6c5f67d1328131f3c044f0793c8ece7e8e64 [formerly bcce976a8a4daccecffa98a73d5d7e590b5533d5]
    Former-commit-id: cb9c1d3801deed12291a5925b32e3cfdb7d3c0a7
    Former-commit-id: d0dd85e8da4ef844b79601f353d2b9c018d036a5

--- .config/ms-windows/autostart.bat
+++ .config/ms-windows/autostart.bat
@@ -12,20 +12,12 @@ if exist "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe" (
 	start /min "" "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe"
 )
 
-if exist "C:\Program Files\pia_manager\pia_manager.exe" (
-	start /min "" "C:\Program Files\pia_manager\pia_manager.exe"
-)
-
 if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Skype for Business 2016.lnk" (
 	start /min "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Skype for Business 2016.lnk"
 )
 
 if exist "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk" (
 	start /min "" "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk"
-)
-
-if exist "C:\Program Files\Private Internet Access\pia-client.exe" (
-	start /min "" "C:\Program Files\Private Internet Access\pia-client.exe"
 )
 
 if exist "C:\Program Files\LGHUB\lghub.exe" (
