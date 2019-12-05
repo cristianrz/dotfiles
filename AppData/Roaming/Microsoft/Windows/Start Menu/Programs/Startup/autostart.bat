b418d745003f11fc5567cd5e7d89c8f03200e19e
cristianarbe <c.ariza@posteo.net>
2019-12-05 12:31:33 +0000

    Fix windows startup programs
    Former-commit-id: 1b16f17ad7c35e3ea2b76cb615ca31cd20cedd54 [formerly e659a2394acb70de16be5d1c72f09488fc7a831c]
    Former-commit-id: 46b7544beebfb6fec9972b61895e8f52f8665a34
    Former-commit-id: bb866d02f495fced0249f445fd9f8fe6250b0f09

--- AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autostart.bat
+++ AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/autostart.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 :PROMPT
-SET /P AREYOUSURE=Do you want to run startup programs? 
+SET /P AREYOUSURE=Run startup programs? 
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 if exist "C:\Program Files (x86)\Trend Micro\OfficeScan Client\PccNTMon.exe" (
@@ -13,11 +13,19 @@ if exist "C:\Program Files\Duplicati 2\Duplicati.GUI.TrayIcon.exe" (
 )
 
 if exist "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk" (
-	start /min "" "C:\Users\%USERNAME%\.config\ms-windows\autohotkey\keyboard-shortcuts.ahk"
+	start /min "" "C:\Users\%USERNAME%\AppData\Roaming\autohotkey\keyboard-shortcuts.ahk"
 )
 
 if exist "C:\Program Files\LGHUB\lghub.exe" (
 	start /min "" "C:\Program Files\LGHUB\lghub.exe"
+)
+
+if exist "C:\Program Files\KeePassXC\KeePassXC.exe" (
+	start /min "" "C:\Program Files\KeePassXC\KeePassXC.exe"
+)
+
+if exist "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Brave Apps\Trello.lnk" (
+	start /min "" "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Brave Apps\Trello.lnk"
 )
 
 timeout 5
@@ -26,8 +34,8 @@ if exist "C:\Program Files\VeraCrypt\VeraCrypt.exe" (
 	start "" "C:\Program Files\VeraCrypt\VeraCrypt.exe"
 )
 
-if exist "C:\Users\%USERNAME%\.config\ms-windows\autostart-assistant.ahk" (
-	Start "" AutoHotkey.exe "C:\Users\%USERNAME%\.config\ms-windows\autostart-assistant.ahk"
+if exist "C:\Users\%USERNAME%\AppData\Roaming\autostart-assistant.ahk" (
+	Start "" AutoHotkey.exe "C:\Users\%USERNAME%\AppData\Roaming\autostart-assistant.ahk"
 )
 
 :END
